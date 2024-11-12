e0a014182f83fdeea23a7a519a79df9d1d1f5131
Nikita <nikondratoriginal@gmail.com>
2024-11-12 19:34:08 +0300

    fix

--- lib/src/feature/profile/widgets/profile_photo.dart
+++ lib/src/feature/profile/widgets/profile_photo.dart
@@ -2,7 +2,8 @@ import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:mama/src/core/core.dart';
+import 'package:mama/src/data.dart';
+import 'package:provider/provider.dart';
 
 class ProfilePhoto extends StatelessWidget {
   const ProfilePhoto({super.key});
